b61c503467dfbbf5cd6f8e1fe85aa2bc10ea8904
servilla <mark.servilla@gmail.com>
2019-05-21 12:19:16 -0600

    Use Config properties

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -44,15 +44,14 @@ class TestPastaCryptp(unittest.TestCase):
 
     def testVerifyAuthtoken(self):
         public_key = pasta_crypto.import_key(self._public_key)
-        authtoken = 'dWlkPUVESSxvPUVESSxkYz1lZGlyZXBvc2l0b3J5LGRjPW9yZypodHRwczovL3Bhc3RhLmVkaXJlcG9zaXRvcnkub3JnL2F1dGhlbnRpY2F0aW9uKjE1NTgwOTA3MDM5NDYqYXV0aGVudGljYXRlZA==-yUoVTpyVityVkfqOpGSPosJYzndBMdwoUTGB0osuqyCNOouPxRllz/pRklaEWqi+faNLGHh8Dzh7qrtxTLLDs+MpBXudaJIIQep6PNnvEDgasrTvA9KV/vnKsyDnu4VaJnyuoKGRryP6PXlJs8UTXhtGpRf2vnTM/oifeRx0NB3y7aEv3Xn85ogxl0MaeyXJFeQMAAyN9ahYgJUC4jFgCqYlLj/x0PAlXwq2C/AwnjC/XJ2mxEQm1E/RMY9Z9EjHx+dSruXEs3wQiBbnus7BPvJR84zqEjl3EYpYwmYRkLViDHYoGdbegcDfuUfKv4y5Hun+r0ICNt09nBV4wci3TQ=='
+        authtoken = Config.TEST_AUTH_TOKEN
         pasta_crypto.verify_authtoken(public_key, authtoken)
 
     def testCreateAuthtoken(self):
         private_key = pasta_crypto.import_key(self._private_key)
-        token = 'uid=EDI,o=EDI,dc=edirepository,dc=org*https://pasta.edirepository.org/authentication*1558090703946*authenticated'
+        token = Config.TEST_TOKEN
         authtoken = pasta_crypto.create_authtoken(private_key, token)
         self.assertIsInstance(authtoken, str)
-        print(authtoken)
 
 
 if __name__ == '__main__':
