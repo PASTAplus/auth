000cd23370b6b0b5b0bf1731c2a810844d684e40
servilla <mark.servilla@gmail.com>
2019-05-21 16:47:00 -0600

    Reconfigure for public user

--- webapp/routes.py
+++ webapp/routes.py
@@ -29,23 +29,25 @@ app.config.from_object(Config)
 
 @app.route('/auth/login', methods=['GET'])
 def login():
+    uid = Config.PUBLIC
+    token = pasta_token.PastaToken()
+    token.system = Config.SYSTEM
     authorization = request.headers.get('Authorization')
-    credentials = base64.b64decode(authorization.strip('Basic ')).decode('utf-8')
-    dn, password = credentials.split(':')
-
-    if pasta_ldap.bind(dn, password):
-        token = pasta_token.PastaToken()
-        token.uid = dn
-        token.system = Config.SYSTEM
-        token.groups = Config.AUTH_GROUP
-        private_key = pasta_crypto.import_key(Config.PRIVATE_KEY)
-        auth_token = pasta_crypto.create_authtoken(private_key, token.to_string())
-        resp = make_response()
-        resp.set_cookie('auth-token', auth_token)
-        return resp
-    else:
-        resp = f'Authentication failed for user: {dn}'
-        return resp, 401
+    if authorization is not None:
+        credentials = base64.b64decode(authorization.strip('Basic ')).decode('utf-8')
+        dn, password = credentials.split(':')
+        if pasta_ldap.bind(dn, password):
+            uid = dn
+            token.groups = Config.AUTH_GROUP
+        else:
+            resp = f'Authentication failed for user: {dn}'
+            return resp, 401
+    token.uid = uid
+    private_key = pasta_crypto.import_key(Config.PRIVATE_KEY)
+    auth_token = pasta_crypto.create_authtoken(private_key, token.to_string())
+    resp = make_response()
+    resp.set_cookie('auth-token', auth_token)
+    return resp
 
 
 def main():
