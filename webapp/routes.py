965846a0d1d02fb5827121f6f86539f306ed36b3
servilla <mark.servilla@gmail.com>
2019-10-01 14:01:01 -0600

    Add support for Orcid user authentication; minor format edits

--- webapp/routes.py
+++ webapp/routes.py
@@ -70,9 +70,9 @@ def login(idp):
         if target is None:
             resp = f'Target parameter not set'
             return resp, 400
-        github_client_id, github_client_secret = get_github_client_info(target)
+        github_client_id, _ = get_github_client_info(target)
         client = WebApplicationClient(github_client_id)
-        github_provider_cfg = get_github_provider_cfg()
+        # github_provider_cfg = get_github_provider_cfg()
         authorization_endpoint = Config.GITHUB_AUTH_ENDPOINT
         redirect_uri = f'{request.base_url}/callback/{target}'
         request_uri = client.prepare_request_uri(
@@ -80,6 +80,17 @@ def login(idp):
             redirect_uri=redirect_uri,
             scope=["user"],
         )
+        return redirect(request_uri)
+    elif idp == 'orcid':
+        if target is None:
+            resp = f'Target parameter not set'
+            return resp, 400
+        orcid_client_id = Config.ORCID_CLIENT_ID
+        authorization_endpoint = Config.ORCID_IMPLICIT_ENDPOINT
+        redirect_uri = f'{request.base_url}/callback/{target}'
+        request_uri = f'{authorization_endpoint}?client_id={orcid_client_id}' + \
+                      f'&response_type=code&scope=/authenticate&' + \
+                      f'redirect_uri={redirect_uri}'
         return redirect(request_uri)
     else:
         resp = f'Unknown identity provider: {idp}'
@@ -106,7 +117,8 @@ def github_callback(target):
         auth=(github_client_id, github_client_secret),
     ).json()
     access_token = token_response['access_token']
-    userinfo_endpoint = f'{Config.GITHUB_USER_ENDPOINT}?access_token={access_token}'
+    userinfo_endpoint = f'{Config.GITHUB_USER_ENDPOINT}' + \
+                        f'?access_token={access_token}'
     userinfo_response = requests.get(url=userinfo_endpoint).json()
     html_url = userinfo_response['html_url']
     auth_token = make_pasta_token(uid=html_url, groups=Config.AUTHENTICATED)
@@ -121,11 +133,11 @@ def github_callback(target):
     return redirect(redirect_url)
 
 
-@app.route("/auth/login/google/callback/<target>", methods=['GET'])
+@app.route('/auth/login/google/callback/<target>', methods=['GET'])
 def google_callback(target):
-    code = request.args.get("code")
+    code = request.args.get('code')
     google_provider_cfg = get_google_provider_cfg()
-    token_endpoint = google_provider_cfg["token_endpoint"]
+    token_endpoint = google_provider_cfg['token_endpoint']
     client = WebApplicationClient(Config.GOOGLE_CLIENT_ID)
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
@@ -140,16 +152,16 @@ def google_callback(target):
         auth=(Config.GOOGLE_CLIENT_ID, Config.GOOGLE_CLIENT_SECRET),
     )
     client.parse_request_body_response(json.dumps(token_response.json()))
-    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    userinfo_endpoint = google_provider_cfg['userinfo_endpoint']
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body)
 
-    if userinfo_response.json().get("email_verified"):
-        unique_id = userinfo_response.json()["sub"]
-        email = userinfo_response.json()["email"]
-        picture = userinfo_response.json()["picture"]
-        given_name = userinfo_response.json()["given_name"]
-        sur_name = userinfo_response.json()["family_name"]
+    if userinfo_response.json().get('email_verified'):
+        unique_id = userinfo_response.json()['sub']
+        email = userinfo_response.json()['email']
+        picture = userinfo_response.json()['picture']
+        given_name = userinfo_response.json()['given_name']
+        sur_name = userinfo_response.json()['family_name']
         auth_token = make_pasta_token(uid=email, groups=Config.AUTHENTICATED)
         common_name = f'{given_name} {sur_name}'
     else:
@@ -159,7 +171,34 @@ def google_callback(target):
     redirect_url = make_target_url(target,
                                    urllib.parse.quote(auth_token),
                                    urllib.parse.quote(common_name))
-    # redirect_url = f'{url_for("show_me")}?uid={users_name}'
+    return redirect(redirect_url)
+
+
+@app.route('/auth/login/orcid/callback/<target>', methods=['GET'])
+def orcid_callback(target):
+    code = request.args.get('code')
+    orcid_client_id = Config.ORCID_CLIENT_ID
+    orcid_client_secret = Config.ORCID_CLIENT_SECRET
+    token_endpoint = Config.ORCID_TOKEN_ENDPOINT
+    body = f'client_id={orcid_client_id}' + \
+           f'&client_secret={orcid_client_secret}' + \
+           f'&grant_type=authorization_code&code={code}' + \
+           f'&redirect_uri={urllib.parse.quote(request.base_url)}'
+    headers = dict()
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    headers['Accept'] = 'application/json'
+    token_response = requests.post(
+        token_endpoint,
+        headers=headers,
+        data=body,
+    ).json()
+    common_name = token_response['name']
+    orcid = Config.ORCID_DNS + token_response['orcid']
+    auth_token = make_pasta_token(uid=orcid, groups=Config.AUTHENTICATED)
+
+    redirect_url = make_target_url(target,
+                                   urllib.parse.quote(auth_token),
+                                   urllib.parse.quote(common_name))
     return redirect(redirect_url)
 
 
@@ -208,5 +247,5 @@ def make_target_url(target, auth_token, common_name):
     return url
 
 
-if __name__ == "__main__":
-    app.run(ssl_context="adhoc")
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc')
