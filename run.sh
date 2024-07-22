e2a685a9ff710a23a32a6b181fad45e798046da4
rogerdahl <git@dahlsys.com>
2024-07-22 09:34:09 -0600

    wip

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This entry point is for development only.
+
 #  --ssl-keyfile /etc/ssl/private/ssl-cert-snakeoil.key \
 #  --ssl-certfile /etc/ssl/certs/ssl-cert-snakeoil.pem
 
