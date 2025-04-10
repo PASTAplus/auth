e905d4992bc327fe5d7ef4216215a0ad447c0966
rogerdahl <git@dahlsys.com>
2025-04-10 13:58:34 -0600

    Add support for showing only a single type of resources in the Permissions page #101

--- webapp/static/js/base.js
+++ webapp/static/js/base.js
@@ -20,35 +20,16 @@ function getPageName() {
 
 // Handle PASTA ID copy buttons
 // This is a global event listener in order to handle dynamically created elements.
-document.addEventListener('click', function(event) {
-  const idEl = event.target.closest('.pasta-id-parent');
-  if (!idEl) { return; }
-  const textEl = idEl.querySelector('.pasta-id-child-text');
-  const pastaIdStr = textEl.textContent.trim();
-  copyTextToClipboard(pastaIdStr);
+document.addEventListener('click', function(ev) {
+  if (ev.target.matches('.pasta-id-copy-button')) {
+    const idEl = ev.target.closest('.pasta-id-parent');
+    const textEl = idEl.querySelector('.pasta-id-child-text');
+    const pastaIdStr = textEl.textContent.trim();
+    navigator.clipboard.writeText(pastaIdStr).catch(function (error) {
+      errorDialog(error);
+    });
+  }
 });
-
-function copyTextToClipboard(text)
-{
-  navigator.clipboard.writeText(text).then(function () {
-    console.log('Text copied to clipboard');
-  }).catch(function (err) {
-    console.error('Could not copy text: ', err);
-  });
-}
-
-// Privacy Policy Modal
-
-let privacyPolicyModal = document.getElementById('privacyPolicyModal');
-if (privacyPolicyModal.dataset.profileId !== undefined && privacyPolicyModal.dataset.policyAccepted !== 'true') {
-  const modal = new bootstrap.Modal(privacyPolicyModal);
-  modal.show();
-  // privacyPolicyModal.addEventListener('hidden.bs.modal', function() {
-  //   let profileId = privacyPolicyModal.dataset.profileId;
-  //   let url = `/profile/${profileId}/privacy_policy`;
-  //   window.location.href = url;
-  // });
-}
 
 // Keep track of the height of the navbar, for use in CSS that limits
 // the height of the main content area.
@@ -57,8 +38,6 @@ function updateNavbarHeight() {
   const navbar = document.querySelector('.navbar');
   const navbarHeight = navbar.offsetHeight;
   document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
-  const headerHeight = getComputedStyle(document.documentElement).getPropertyValue('--navbar-height');
-  console.log('Header Height:', headerHeight);
 }
 
 updateNavbarHeight();
