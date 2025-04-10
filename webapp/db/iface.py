628693e35927214d3bcf47c1d675b6c2d6579e6b
rogerdahl <git@dahlsys.com>
2025-04-10 12:41:03 -0600

    Move to database triggers to keep in-memory caches up to date #95

--- webapp/db/iface.py
+++ webapp/db/iface.py
@@ -1,7 +1,4 @@
-import sqlite3
-
 import daiquiri
-import fastapi
 import sqlalchemy.event
 import sqlalchemy.orm
 import sqlalchemy.pool
@@ -16,7 +13,6 @@ from config import Config
 
 log = daiquiri.getLogger(__name__)
 
-UserDb = db.user.UserDb
 
 engine = sqlalchemy.create_engine(
     sqlalchemy.engine.URL.create(
@@ -36,29 +32,79 @@ engine = sqlalchemy.create_engine(
 # Create the tables in the database
 db.base.Base.metadata.create_all(engine)
 
-SessionLocal = sqlalchemy.orm.sessionmaker(
-    autocommit=False, autoflush=False, bind=engine
-)
+SessionLocal = sqlalchemy.orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-# Enable foreign key checking in SQLite
-@sqlalchemy.event.listens_for(sqlalchemy.pool.Pool, 'connect')
-def _on_connect(dbapi_con, _connection_record):
-    if isinstance(dbapi_con, sqlite3.Connection):
-        dbapi_con.execute('PRAGMA foreign_keys=ON')
+def create_sync_trigger(dbapi_connection, _connection_record):
+    """Create triggers to track table changes for synchronization with in-memory caches."""
+    cursor = dbapi_connection.cursor()
+    try:
+        # Create trigger function. This function is called by the triggers to update the sync table.
+        # A row for the given table name is created if it does not exist, or updated if it does.
+        # In both cases, the updated timestamp is set to the current datetime.
+        cursor.execute(
+            """
+            do $$
+            begin
+                if not exists (select 1 from pg_proc where proname = 'sync_trigger_func') then
+                    create or replace function sync_trigger_func()
+                    returns trigger
+                    language plpgsql
+                    as $body$
+                    begin
+                        RAISE NOTICE 'Sync trigger for table: %', TG_TABLE_NAME;
+        
+                        insert into sync (name, updated)
+                        values (TG_TABLE_NAME, now())
+                        on conflict (name)
+                        do update set updated = excluded.updated;
+                        
+                        return null;
+                    end;
+                    $body$;
+                end if;
+            end;
+            $$;
+            """
+        )
+
+        # Create triggers for each table
+        for trigger_name, table_name in (
+            ("sync_trigger_collection", "collection"),
+            ("sync_trigger_group", "\"group\""),
+            ("sync_trigger_group_member", "group_member"),
+            ("sync_trigger_identity", "identity"),
+            ("sync_trigger_rule", "rule"),
+            ("sync_trigger_profile", "profile"),
+            ("sync_trigger_resource", "resource"),
+        ):
+            cursor.execute(
+                f"""
+                do $$
+                begin
+                    if not exists (select 1 from pg_trigger where tgname = '{trigger_name}') then
+                        create trigger {trigger_name}
+                        after insert or update on {table_name}
+                        for each statement
+                        execute function sync_trigger_func();
+                    end if;
+                end;
+                $$;
+                """
+            )
+
+        dbapi_connection.commit()
+    finally:
+        cursor.close()
+
+
+sqlalchemy.event.listen(engine, 'connect', create_sync_trigger)
 
 
 def get_session():
     session = SessionLocal()
     try:
         yield session
-    finally:
-        session.close()
-
-
-def udb(session: sqlalchemy.orm.Session = fastapi.Depends(get_session)):
-    try:
-        yield db.user.UserDb(session)
     finally:
         session.close()
 
--- webapp/util/search_cache.py
+++ webapp/util/search_cache.py
@@ -1,9 +1,9 @@
 """Cache profiles and groups in memory for fast searching.
 
-We dynamically update search results as the user types in the search box. In order to speed
-up this search and avoid hitting the database, we cache the profiles and groups in memory.
+We dynamically update search results as the user types in the search box. In order to speed up this
+search and avoid hitting the database each time the user presses a key, we cache the profiles and
+groups in memory.
 """
-import pprint
 import re
 
 import daiquiri
@@ -16,7 +16,8 @@ log = daiquiri.getLogger(__name__)
 
 cache = {
     'sync_ts': None,
-    'candidate_list': [],
+    'profile_list': [],
+    'group_list': [],
 }
 
 
@@ -35,24 +36,23 @@ async def init_profiles(udb):
         if profile_row.pasta_id in Config.SUPERUSER_LIST:
             continue
         key_tup = (
-            profile_row.given_name,
-            profile_row.family_name,
             profile_row.full_name,
+            profile_row.family_name,
             profile_row.email,
             profile_row.pasta_id,
             # Enable searching for the PASTA ID without the 'PASTA-' prefix
             re.sub(r'^PASTA-', '', profile_row.pasta_id),
         )
-        candidate_list.append(
+        profile_list.append(
             (
                 tuple(k.lower() for k in key_tup if k is not None),
                 {
-                    'id': profile_row.id,
+                    'principal_id': principal_row.id,
+                    'principal_type': 'profile',
                     'pasta_id': profile_row.pasta_id,
                     'title': profile_row.full_name,
-                    'descr': profile_row.email,
+                    'description': profile_row.email,
                     'avatar_url': profile_row.avatar_url,
-                    'type': 'profile',
                 },
             )
         )
@@ -67,12 +67,14 @@ async def init_groups(udb):
             group_row.description,
             group_row.pasta_id,
             re.sub(r'^PASTA-', '', group_row.pasta_id),
+            'group',
         )
-        candidate_list.append(
+        group_list.append(
             (
                 tuple(k.lower() for k in key_tup if k is not None),
                 {
-                    'id': group_row.id,
+                    'principal_id': group_row.id,
+                    'principal_type': 'group',
                     'pasta_id': group_row.pasta_id,
                     'title': group_row.name,
                     'description': (group_row.description or ''),
@@ -95,33 +97,23 @@ async def search(query_str, include_groups):
 
     match_list = []
 
-    for key_tup, v in cache['candidate_list']:
-        if is_match(query_str, key_tup):
-            match_list.append(v)
+    # The keys are stored in lower case.
+    lower_str = query_str.lower()
+    for key_tup, principal_dict in cache['profile_list']:
         if len(match_list) >= Config.SEARCH_LIMIT:
             break
+        if any(k.startswith(lower_str) for k in key_tup):
+            match_list.append(principal_dict)
+
+    if include_groups:
+        for key_tup, principal_dict in cache['group_list']:
+            if len(match_list) >= Config.SEARCH_LIMIT:
+                break
+            if any(k.startswith(lower_str) for k in key_tup):
+                match_list.append(principal_dict)
 
     # log.debug(f'match_list:')
     # for m in match_list:
     #     log.debug(f'  {m}')
 
     return match_list
-
-
-def get_score(query_str, key_tup):
-    """Return the score of the query string against the key tuple.
-
-    Strategy: Count the number of shared characters between query_str and each string in the
-    key_tup, and score each matching character as 100. Then subtract the length of query_str in
-    order to penalize strings with more non-matching characters.
-    """
-    return max(
-        sum(100 for c1, c2 in zip(k, query_str) if c1 == c2) - len(query_str)
-        for k in key_tup
-        if k is not None
-    )
-
-
-def is_match(query_str, key_tup):
-    """Return True if one or more of the key_tup strings start with the query_str."""
-    return any(k.startswith(query_str) for k in key_tup if k is not None)
