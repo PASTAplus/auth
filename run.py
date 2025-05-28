b6f65b2ca360e4469db2620ad8d67b1eb291de04
rogerdahl <git@dahlsys.com>
2025-05-28 15:34:06 -0600

    Update run.py

--- run.py
+++ run.py
@@ -2,30 +2,33 @@
 
 See wsgi.py for the main entry point for production.
 """
+import pathlib
+import sys
+
 import daiquiri
 import uvicorn
-import sys
-import pathlib
 
 ROOT_PATH = pathlib.Path(__file__).resolve().parent
 sys.path.append((ROOT_PATH / 'webapp').as_posix())
 
-from config import Config
-from fastapi_app import app
+import config
+import fastapi_app
+
+import main
 
 daiquiri.setup(
-    level=Config.LOG_LEVEL,
+    level=config.Config.LOG_LEVEL,
     outputs=(
-        daiquiri.output.File(Config.LOG_PATH),
+        daiquiri.output.File(config.Config.LOG_PATH),
         'stdout',
     ),
 )
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        fastapi_app.app,
         host='127.0.0.1',
         port=5443,
-        ssl_keyfile=Config.TLS_KEY_PATH,
-        ssl_certfile=Config.TLS_CERT_PATH,
+        ssl_keyfile=config.Config.TLS_KEY_PATH,
+        ssl_certfile=config.Config.TLS_CERT_PATH,
     )
