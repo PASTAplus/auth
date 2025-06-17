2265aa45b8d0df72667a28fd81f2aa417722f768
rogerdahl <git@dahlsys.com>
2025-06-17 07:28:19 -0600

    Update unit test framework (wip)

--- sample.py
+++ sample.py
@@ -7,8 +7,21 @@ HERE_PATH = pathlib.Path(__file__).parent.resolve()
 TEST_FILES_PATH = HERE_PATH / 'tests' / 'test_files'
 
 
+def assert_equal(got_str, filename):
+    """Assert that the string is identical to the contents of the sample file"""
+    expected_str = _read_file(filename)
+    if got_str != expected_str:
+        print('Got:')
+        print(got_str)
+        print('Expected:')
+        print(expected_str)
+        # assert False
+        meld(got_str, filename)
+        # assert got_str == expected_str
+
+
 def assert_equal_json(got_json_str, filename):
-    """Assert that the JSON string is semantically identical to the contents of the file"""
+    """Assert that the JSON string is semantically identical to the contents of the sample file"""
     norm_json_str = _normalize_json(got_json_str)
     expected_json_str = _read_file(filename)
     # assert norm_json_str == expected_json_str
