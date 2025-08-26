f83e14a1f89d0a58a81ed993ef9bd1d8b7f95932
rogerdahl <git@dahlsys.com>
2025-08-26 11:41:57 -0600

    Add version number to HTML asset URLs #164

--- webapp/util/template.py
+++ webapp/util/template.py
@@ -7,9 +7,9 @@ from config import Config
 templates = starlette.templating.Jinja2Templates(Config.TEMPLATES_PATH)
 templates.env.globals.update(
     {
-        # Make the url() function available in all templates
+        # Make these functions and other objects available in all templates
         'url': util.url.url,
-        # Parameters for base.html
+        'url_buster': util.url.url_buster,
         'dev_menu': Config.ENABLE_DEV_MENU,
         'IdpName': db.models.identity.IdpName,
     }
