d0324edd2054eecf66d6670bb916d13e8cfe4824
rogerdahl <git@dahlsys.com>
2025-09-01 19:38:58 -0600

    Refactor PASTA JWTs as EDI tokens #174
    These changes make a cleaner interface for creating and consuming EDI tokens. The methods that encode and decode EDI tokens are now separate from the token representation itself. It also formalizes all the claims in the token.

--- cli/add_eml_docs.py
+++ cli/add_eml_docs.py
@@ -2,9 +2,10 @@
 
 """Add EML documents for testing.
 
-- This will search a local dir hierarchy for EML documents and add them to running EDI
-IAM instance (local by default).
-- Only EDI Level-1 EML documents are supported (filename: Level-1-EML.xml).
+- This uses the AddEML API.
+- If the path to a directory tree is provided, this will recursively search for EML documents named
+Level-1-EML.xml and add them to the running EDI IAM instance (local by default).
+- If the path to a single EML document is provided, it will add just that document.
 - The token profile must be a member of the Vetted system group.
 - The token profile becomes the owner of all the resources created from the EML document. This
 differs from production, where EML document are usually owned by their uploading profiles.
@@ -37,9 +38,9 @@ def main():
         help='Path to file containing EDI token',
     )
     parser.add_argument(
-        'root_path',
+        'eml_path',
         type=pathlib.Path,
-        help='Path to root of directory tree containing EML files',
+        help='Path to an EML document, or to the root of a directory tree containing EML files',
     )
     parser.add_argument(
         '--key_prefix',
@@ -66,25 +67,45 @@ def main():
 
     token_str = args.token_path.read_text()
 
-    for eml_path in args.root_path.rglob('Level-1-EML.xml'):
-        log.info(
-            f'Adding EML document: {eml_path.as_posix()}',
-        )
-        response = requests.post(
-            DEFAULT_ADD_EML_ENDPOINT,
-            json={
-                'eml': eml_path.read_text(),
-                'key_prefix': args.key_prefix,
-            },
-            cookies={
-                'edi-token': token_str,
-            },
-            # Disable SSL verification for local testing
-            verify=False,
-        )
-        if response.status_code != 200:
-            log.error(f'Failed to add EML document')
-            log.error(pprint.pformat(response.json()))
+    if args.eml_path.is_file():
+        return add_eml(token_str, args, args.eml_path)
+    elif args.eml_path.is_dir():
+        add_eml_from_directory_tree(token_str, args)
+        return 0
+    else:
+        log.error(f'Path is not a file or directory: {args.eml_path.as_posix()}')
+        return 1
+
+
+def add_eml_from_directory_tree(token_str, args):
+    for eml_path in args.eml_path.rglob('Level-1-EML.xml'):
+        add_eml(token_str, args, eml_path)
+
+
+def add_eml(token_str, args, eml_path):
+    log.info(
+        f'Adding EML document: {eml_path.as_posix()}',
+    )
+    response = requests.post(
+        args.endpoint,
+        json={
+            'eml': eml_path.read_text(),
+            'key_prefix': args.key_prefix,
+        },
+        cookies={
+            'edi-token': token_str,
+        },
+        # Disable SSL verification for local testing
+        verify=False,
+    )
+    if response.status_code != 200:
+        log.error(f'Failed to add EML document - HTTP {response.status_code}')
+        log.error(pprint.pformat(response.json()))
+        return 1
+
+    log.info('EML document added successfully')
+    log.info(pprint.pformat(response.json()))
+    return 0
 
 
 if __name__ == '__main__':
