5c42890fc66591dfff36ddd6c14ff56965800ddd
rogerdahl <git@dahlsys.com>
2025-09-29 09:15:49 -0600

    Add token refresh to UI #179

--- webapp/main.py
+++ webapp/main.py
@@ -1,5 +1,7 @@
 import logging
 import pathlib
+import re
+import time
 
 import daiquiri.formatter
 import fastapi
@@ -16,6 +18,7 @@ import api.v1.profile
 import api.v1.resource
 import api.v1.rule
 import api.v1.token
+import db.models.profile
 import idp.github
 import idp.google
 import idp.ldap
@@ -136,12 +139,73 @@ create_route('/manifest.json', Config.STATIC_PATH / 'site/manifest.json')
 
 # Middleware
 
+# Note: Middleware is executed in reverse order of addition.
+
+
+class RedirectToSigninMiddleware(starlette.middleware.base.BaseHTTPMiddleware):
+    """
+    Middleware to redirect unauthenticated users to the sign-in page.
+    - If a request is made to a '/ui' path without a valid token, the user is redirected to
+    '/signout', which removes the invalid (usually expired) cookie, and which then redirects to
+    '/ui/signin'.
+    - For any other request without a valid token, a 401 Unauthorized response is returned.
+    """
+
+    async def dispatch(self, request: starlette.requests.Request, call_next):
+        if re.match(fr'^{util.url.url("/ui")}(?!/(?:signin(?!/link)|help|api/))', request.url.path):
+            if request.state.claims is None:
+                # log.debug('Redirecting to /ui/signin: UI page requested without valid token')
+                return util.redirect.internal('/signout', info='expired')
+
+        return await call_next(request)
+
+
+app.add_middleware(RedirectToSigninMiddleware)
+
+
+class TokenProfileMiddleware(starlette.middleware.base.BaseHTTPMiddleware):
+    """Middleware to decode the EDI token from the request cookie.
+    - If the token is missing or invalid, the token claims is set to None.
+    - If the token is older than the refresh delta, but still valid, it is refreshed by adding a new
+    token cookie to the response.
+    - The token claims is stored in the request state for use by downstream handlers.
+    """
+
+    async def dispatch(self, request: starlette.requests.Request, call_next):
+        claims_obj = None  # type: util.edi_token.EdiTokenClaims | None
+
+        token_str = request.cookies.get('edi-token')
+        if token_str is not None:
+            # Note: It's important to not run this code for the unit tests, as it creates a separate
+            # session in which the test profiles don't exist, which causes the token to be invalid.
+            async with util.dependency.get_dbi() as dbi:
+                claims_obj = await util.edi_token.decode(dbi, token_str)
+                # A DB row is only valid within the session it was created in, so we cannot store a
+                # profile_row here for use in downstream handlers.
+
+        request.state.claims = claims_obj
+        response = await call_next(request)
+
+        if (
+            claims_obj is not None
+            and time.time() - claims_obj.iat > Config.JWT_REFRESH_DELTA.total_seconds()
+        ):
+            async with util.dependency.get_dbi() as dbi:
+                profile_row = await dbi.get_profile(claims_obj.edi_id)
+                new_token = await util.edi_token.create(dbi, profile_row)
+            response.set_cookie('edi-token', new_token)
+
+        return response
+
+
+app.add_middleware(TokenProfileMiddleware)
+
 
 class RootPathMiddleware(starlette.middleware.base.BaseHTTPMiddleware):
     """Middleware to set the root path for the application.
-    This middleware ensures that the application routes are agnostic of the root path
-    it is being served from. It sets the root_path in the ASGI request scope.
-    In addition, it redirects requests that do not start with the root path to the root path.
+    - This middleware ensures that the application routes are agnostic of the root path it is being
+    served from. It sets the root_path in the ASGI request scope.
+    - In addition, it redirects requests that do not start with the root path to the root path.
     """
 
     async def dispatch(self, request: starlette.requests.Request, call_next):
@@ -151,41 +215,8 @@ class RootPathMiddleware(starlette.middleware.base.BaseHTTPMiddleware):
         return await call_next(request)
 
 
-class RedirectToSigninMiddleware(starlette.middleware.base.BaseHTTPMiddleware):
-    """
-    Middleware to redirect unauthenticated users to the sign-in page.
-
-    If a request is made to a '/ui' path without a valid token, the user is redirected to
-    '/signout', which removes the invalid (usually expired) cookie, and which then redirects to
-    '/ui/signin'.
-
-    For any other request without a valid token, a 401 Unauthorized response is returned.
-    """
-
-    async def dispatch(self, request: starlette.requests.Request, call_next):
-        if (
-            request.url.path.startswith(str(util.url.url('/ui')))
-            and not request.url.path.startswith(str(util.url.url('/ui/signin')))
-            and not request.url.path.startswith(str(util.url.url('/ui/help')))
-            and not request.url.path.startswith(str(util.url.url('/ui/api/')))
-        ):
-            # Note: It's important to not run this code for the unit tests, as it creates a separate
-            # session in which is_valid() will not find the test profiles.
-            async with util.dependency.get_dbi() as dbi:
-                if not await util.edi_token.is_valid(
-                    dbi, request.cookies.get('edi-token')
-                ):
-                    log.debug('Redirecting to /ui/signin: UI page requested without valid token')
-                    return util.redirect.internal('/signout')
-
-        return await call_next(request)
-
-
-# Add middleware to the application
-# noinspection PyTypeChecker
 app.add_middleware(RootPathMiddleware)
-# noinspection PyTypeChecker
-app.add_middleware(RedirectToSigninMiddleware)
+
 
 # Include all routers
 app.include_router(api.v1.eml.router)
